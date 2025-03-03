ec54038645f91cb9837ec53cb0ccfe5b62e0d840
unknown <gevorgalexanyan04@gmail.com>
2025-03-03 21:48:36 +0400

    added js

---
+++ Gevorg_Alexanyan/script.js
@@ -0,0 +1,29 @@
+console.log("Hello World");
+
+// alert("Welcome");
+
+// prompt();
+
+// confirm("bdh igbkd");
+
+// let karmir = "red"
+
+// console.log(karmir);
+
+//  karmir = "blue"
+
+//  console.log(karmir);
+
+// let tiv = 1; // int
+// let text = "text"; // string
+// let booi = true // boolen
+
+// const minimalTaric = 18;
+
+let x = prompt("Mutcagreq zer tariqy");
+
+x = Number(x);
+
+alert(x >= 18);
+alert(x*x)
+
