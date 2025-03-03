2daa2ad802a292603dc3e9ea3c9974b26675606c
Aram <arammelkonyan32@gmail.com>
2025-03-03 09:48:05 -0800

    js lesson

---
+++ Aram/script.js
@@ -0,0 +1,21 @@
+console.log("Hello world")
+// alert("welcome")
+// prompt("Hello")
+// confirm("subscribe")
+
+console.log(4 > 8);
+console.log(4 < 8);
+console.log(4 == 8);
+console.log(4 != 8);
+
+
+console.log(4 === "4");
+console.log(true === 1);
+console.log(true !== 1);
+
+
+x = Number(prompt("type the number"))
+
+// alert( x >= 18)
+alert( x * x)
+
