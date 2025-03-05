8ce23a9f52d00e3cc2fb50c5d3d1fab0183cc469
unknown <gevorgalexanyan04@gmail.com>
2025-03-05 22:29:36 +0400

    tnayin

--- Gevorg_Alexanyan/script.js
+++ Gevorg_Alexanyan/script.js
@@ -1,29 +1,5 @@
-console.log("Hello World");
+let a = Number(prompt("Enter Height:"));
+let b = Number(prompt("Enter Width:"));
 
-// alert("Welcome");
-
-// prompt();
-
-// confirm("bdh igbkd");
-
-// let karmir = "red"
-
-// console.log(karmir);
-
-//  karmir = "blue"
-
-//  console.log(karmir);
-
-// let tiv = 1; // int
-// let text = "text"; // string
-// let booi = true // boolen
-
-// const minimalTaric = 18;
-
-let x = prompt("Mutcagreq zer tariqy");
-
-x = Number(x);
-
-alert(x >= 18);
-alert(x*x)
-
+alert("Perimeter: " + (2 * (a + b))); 
+alert("Area: " + (a * b));
