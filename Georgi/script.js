350118fd9519d6894badea5cbb278c6f8d71ecb2
GeorgiGharibyan <gharibyang.01@gmail.com>
2025-03-17 16:05:28 +0400

    Check letter A - a

--- Georgi/script.js
+++ Georgi/script.js
@@ -190,17 +190,109 @@
 
 // erankyun(2, 2, 2)
 
-function armatner(a, b, c) {
-    let d = b * b - 4 * a * c
-    if (d === 0) {
-        alert("armat chka")
-    } else if (d > 0) {
-        let x1 = (-b + Math.sqrt(d)) / (2 * a)
-        let x2 = (-b - Math.sqrt(d)) / (2 * a)
-        alert("x1 = " + x1 + ", x2 = " + x2)
-    } else {
-        let x3 = -b / (2 * a)
-        alert("x1 = x2 =" + x3)
+// function armatner(a, b, c) {
+//     let d = b * b - 4 * a * c
+//     if (d === 0) {
+//         alert("armat chka")
+//     } else if (d > 0) {
+//         let x1 = (-b + Math.sqrt(d)) / (2 * a)
+//         let x2 = (-b - Math.sqrt(d)) / (2 * a)
+//         alert("x1 = " + x1 + ", x2 = " + x2)
+//     } else {
+//         let x3 = -b / (2 * a)
+//         alert("x1 = x2 =" + x3)
+//     }
+// }
+// armatner(1, 4, 4)
+
+// const car = {
+//     anun: "BMW",
+//     sharjich: " 4.4 լ",
+//     nkar: "https://s.list.am/f/514/87115514.webp",
+//     gin: "$65,000",
+//     tari: "2020 թ."
+// }
+// console.log(`${car.anun} ,${car.sharjich}`);
+// console.log(`${car.gin} , ${car.tari}`);
+
+
+// const person = {
+//     lastname: "Doe",
+//     fullname: function (a) {
+//         return `Hello ${a} !!!`
+//     }
+// }
+// console.log(person.fullname("John"));
+
+// const matem = {
+//     astichan: function (a, b) {
+//         return Math.pow(a, b)
+//     },
+//     armat: function (a, b) {
+//         return a ** 1 / b
+//     },
+//     klor: function (a) {
+//         return Math.round(a)
+//     }
+// }
+
+// console.log(matem.astichan(4, 2));
+// console.log(matem.armat(4, 2));
+// console.log(matem.klor(4.3425));
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// const tver = [1, 2, 3, 40, 5, 6, 7, 8]
+// for (let i = 0; i < tver.length; i++) {
+//     if (tver[i] % 2 == 0 && i % 2 != 0) {
+//         console.log(tver[i]);
+//     }
+// }   
+
+// // let n = Number(prompt("mutqagreq qanak"));
+// let tver = []
+// for (let n = Number(prompt("mutqagreq qanak")); n)
+
+// const tver = []
+// let n = Number(prompt("mutqagreq qanak"))
+// let d = Number(prompt("mutqagreq bajanvox tiv"))
+// for (let i = 0; i <= n; i++) {
+//     if (i % d == 0) {
+//         tver.push(i)
+// }
+// console.log(tver);
+
+const names = [
+    "Michael",
+    "Emma",
+    "David",
+    "Sophia",
+    "James",
+    "Olivia",
+    "John",
+    "Isabella",
+    "Robert",
+    "Mia",
+    "William",
+    "Charlotte",
+    "Daniel",
+    "Amelia",
+    "Joseph",
+    "Harper",
+    "Charles",
+    "Evelyn",
+    "Thomas",
+    "Abigail"
+]
+
+for (let i = 0; i < names.length; i++) {
+    const s = names[i];
+    for (let j = 0; j < s.length; j++) {
+        if (s[j] === "A" || s[j] === "a") {
+            console.log(s);
+        }
     }
 }
-armatner(2, 2, 2)
